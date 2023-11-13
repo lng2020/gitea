fcbc5ffe25ffbc840f72d87c384cb78b12e4af38
lng2020 <nanguanlin6@gmail.com>
2023-11-13 17:51:44 +0800

    add project test

--- tests/integration/api_project_test.go
+++ tests/integration/api_project_test.go
@@ -11,6 +11,7 @@ import (
 	"testing"
 
 	auth_model "code.gitea.io/gitea/models/auth"
+	repo_model "code.gitea.io/gitea/models/repo"
 	"code.gitea.io/gitea/models/unittest"
 	user_model "code.gitea.io/gitea/models/user"
 	api "code.gitea.io/gitea/modules/structs"
@@ -18,46 +19,142 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestAPIListUserProjects(t *testing.T) {
-}
-
-func TestAPIListOrgProjects(t *testing.T) {
+func TestAPIListProjects(t *testing.T) {
 	defer tests.PrepareTestEnv(t)()
 
 	user := unittest.AssertExistsAndLoadBean(t, &user_model.User{ID: 2})
 	org := unittest.AssertExistsAndLoadBean(t, &user_model.User{ID: 17})
+	repo := unittest.AssertExistsAndLoadBean(t, &repo_model.Repository{ID: 1})
 
 	session := loginUser(t, user.Name)
-	token := getTokenForLoggedInUser(t, session, auth_model.AccessTokenScopeReadOrganization, auth_model.AccessTokenScopeReadIssue)
-	link, _ := url.Parse(fmt.Sprintf("/api/v1/orgs/%s/projects", org.Name))
+	token := getTokenForLoggedInUser(t, session, auth_model.AccessTokenScopeReadUser, auth_model.AccessTokenScopeReadOrganization, auth_model.AccessTokenScopeReadRepository, auth_model.AccessTokenScopeReadIssue)
 
-	link.RawQuery = url.Values{"token": { token}}.Encode()
+	testCase := []struct {
+		link        string
+		expectedLen int
+	}{
+		{fmt.Sprintf("/api/v1/user/%s/projects", user.Name), 1},
+		{fmt.Sprintf("/api/v1/orgs/%s/projects", org.Name), 1},
+		{fmt.Sprintf("/api/v1/repos/%s/%s/projects", user.Name, repo.Name), 1},
+	}
 
-	req := NewRequest(t, "GET", link.String())
-	var apiProjects []*api.Project
+	for _, test := range testCase {
+		link, _ := url.Parse(test.link)
+		link.RawQuery = url.Values{"token": {token}}.Encode()
 
-	resp := session.MakeRequest(t, req, http.StatusOK)
-	DecodeJSON(t, resp, &apiProjects)
-	assert.Len(t, apiProjects, 1)
-
+		var apiProjects []*api.Project
+		req := NewRequest(t, "GET", link.String())
+		resp := session.MakeRequest(t, req, http.StatusOK)
+		DecodeJSON(t, resp, &apiProjects)
+		assert.Len(t, apiProjects, test.expectedLen)
+	}
 }
 
-func TestAPIListRepoProjects(t *testing.T) {
-}
+func TestAPICreateProject(t *testing.T) {
+	defer tests.PrepareTestEnv(t)()
 
-func TestAPICreateUserProject(t *testing.T) {
-}
+	user := unittest.AssertExistsAndLoadBean(t, &user_model.User{ID: 2})
+	repo := unittest.AssertExistsAndLoadBean(t, &repo_model.Repository{ID: 1})
+	org := unittest.AssertExistsAndLoadBean(t, &user_model.User{ID: 17})
 
-func TestAPICreateOrgProject(t *testing.T) {
-}
+	session := loginUser(t, user.Name)
+	token := getTokenForLoggedInUser(t, session, auth_model.AccessTokenScopeWriteUser, auth_model.AccessTokenScopeWriteRepository, auth_model.AccessTokenScopeWriteIssue)
 
-func TestAPICreateRepoProject(t *testing.T) {
+	payload := api.NewProjectPayload{
+		Title:       "Test Project",
+		Description: "Test Project Description",
+		BoardType:   uint8(1),
+	}
+
+	expectedProject := api.Project{
+		Title:       "Test Project",
+		Description: "Test Project Description",
+		BoardType:   uint8(1),
+	}
+
+	testCase := []struct {
+		link string
+	}{
+		{fmt.Sprintf("/api/v1/user/%s/projects", user.Name)},
+		{fmt.Sprintf("/api/v1/repos/%s/%s/projects", user.Name, repo.Name)},
+		{fmt.Sprintf("/api/v1/orgs/%s/projects", org.Name)},
+	}
+
+	for _, test := range testCase {
+		link, _ := url.Parse(test.link)
+		link.RawQuery = url.Values{"token": {token}}.Encode()
+
+		req := NewRequestWithJSON(t, "POST", link.String(), payload)
+		resp := session.MakeRequest(t, req, http.StatusCreated)
+
+		var apiProject api.Project
+		DecodeJSON(t, resp, &apiProject)
+		assert.Equal(t, expectedProject.Title, apiProject.Title)
+		assert.Equal(t, expectedProject.Description, apiProject.Description)
+		assert.Equal(t, expectedProject.BoardType, apiProject.BoardType)
+	}
 }
 
 func TestAPIGetProject(t *testing.T) {
+	defer tests.PrepareTestEnv(t)()
+
+	user := unittest.AssertExistsAndLoadBean(t, &user_model.User{ID: 2})
+	session := loginUser(t, user.Name)
+	token := getTokenForLoggedInUser(t, session, auth_model.AccessTokenScopeReadUser, auth_model.AccessTokenScopeReadRepository, auth_model.AccessTokenScopeReadIssue)
+
+	testCase := []struct {
+		projectID       int
+		expectedProject api.Project
+	}{
+		{1, api.Project{ID: 1, Title: "First project"}},
+		{2, api.Project{ID: 2, Title: "Second project"}},
+	}
+
+	for _, test := range testCase {
+		link, _ := url.Parse(fmt.Sprintf("/api/v1/projects/%d", test.projectID))
+		link.RawQuery = url.Values{"token": {token}}.Encode()
+
+		req := NewRequest(t, "GET", link.String())
+		resp := session.MakeRequest(t, req, http.StatusOK)
+
+		var apiProject api.Project
+		DecodeJSON(t, resp, &apiProject)
+		assert.Equal(t, test.expectedProject.ID, apiProject.ID)
+		assert.Equal(t, test.expectedProject.Title, apiProject.Title)
+	}
 }
 
 func TestAPIUpdateProject(t *testing.T) {
+	defer tests.PrepareTestEnv(t)()
+
+	user := unittest.AssertExistsAndLoadBean(t, &user_model.User{ID: 2})
+	session := loginUser(t, user.Name)
+	token := getTokenForLoggedInUser(t, session, auth_model.AccessTokenScopeWriteUser, auth_model.AccessTokenScopeWriteRepository, auth_model.AccessTokenScopeWriteIssue)
+
+	payload := api.UpdateProjectPayload{
+		Title:       "Edited test Project",
+		Description: "Edited test Project Description",
+	}
+
+	expectedProject := api.Project{
+		Title:       "Edited test Project",
+		Description: "Edited test Project Description",
+	}
+
+	testCase := []int{1, 2}
+
+	for _, test := range testCase {
+		link, _ := url.Parse(fmt.Sprintf("/api/v1/projects/%d", test))
+		link.RawQuery = url.Values{"token": {token}}.Encode()
+
+		req := NewRequestWithJSON(t, "PATCH", link.String(), payload)
+		resp := session.MakeRequest(t, req, http.StatusOK)
+
+		var apiProject api.Project
+		DecodeJSON(t, resp, &apiProject)
+		assert.Equal(t, expectedProject.Title, apiProject.Title)
+		assert.Equal(t, expectedProject.Description, apiProject.Description)
+	}
 }
 
 func TestAPIDeleteProject(t *testing.T) {
